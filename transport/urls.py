da4b71bb4d9cc81a3ddd760145ac96c685d5bf84
Anish Masand <anish.b.masand@gmail.com>
2023-03-03 15:28:16 +0530

    added all the html files to django

--- transport/urls.py
+++ transport/urls.py
@@ -2,5 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns=[
-    path('landingPage',views.showLandingPage,name="landingPage")
+    path('landingPage',views.showLandingPage,name="landingPage"),
+    path('loginPage',views.showLoginPage,name="loginPage"),
+    path('signupPage',views.showSignupPage,name="signupPage"),
+    path('paymentPage',views.showPaymentPage,name="paymentPage"),
+    path('bookingPage',views.showBookingPage,name="bookingPage"),
+
 ]
--- transport/views.py
+++ transport/views.py
@@ -3,3 +3,11 @@ from django.shortcuts import render
 # Create your views here.
 def showLandingPage(request):
     return render(request,"transport/landingPage.html")
+def showLoginPage(request):
+    return render(request,"transport/loginPage.html")
+def showSignupPage(request):
+    return render(request,"transport/signupPage.html")
+def showPaymentPage(request):
+    return render(request,"transport/paymentPage.html")
+def showBookingPage(request):
+    return render(request,"transport/bookingPage.html")
