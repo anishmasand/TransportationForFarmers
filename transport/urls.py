a9a8d2fd92faf65ceeaf496514ce8c0f8b7acb99
KarinaKarira <karinakarira55@gmail.com>
2023-03-03 15:36:25 +0530

    Connected CSS and JS file to the landing page

--- transport/urls.py
+++ transport/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns=[
-    path('landingPage',views.showLandingPage,name="landingPage")
+    path('landingPage',views.showLandingPage,name="landingPage"),
+    # path('loginPage',views.showLoginPage,name="loginPage")
 ]
