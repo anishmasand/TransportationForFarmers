e9bd0e331f16597975874c50f5b3e939a7ee287f
Anish Masand <anish.b.masand@gmail.com>
2023-03-04 11:48:19 +0530

    error

--- transport/views.py
+++ transport/views.py
@@ -42,7 +42,6 @@ def showDriversInterface(request):
 
 
 def registerFarmer(request):
-<<<<<<< HEAD
     fullname = request.POST['fullname']
     email = request.POST['email']
     password = request.POST['password']
@@ -50,25 +49,16 @@ def registerFarmer(request):
     location = request.POST['location']
     supply_capacity = request.POST['supply_capacity']
     # hashed password
-    hashed_password = make_password(password)
-    farmer = Farmer(name=fullname, email=email, password=hashed_password,
-                    mobile=mobile, location=location, supply_capacity=supply_capacity)
-=======
-    fullname=request.POST['fullname']
-    email=request.POST['email']
-    password=request.POST['password']
-    mobile=request.POST['mobile']
-    location=request.POST['location']
-    supply_capacity=request.POST['supply_capacity']
-    #hashed password
     # hashed_password=make_password(password)
-    farmer=Farmer(name=fullname,email=email,password=password,mobile=mobile,location=location,supply_capacity=supply_capacity)
->>>>>>> 6100641488d31681c58f3805567b853e76fdbde3
+    farmer = Farmer(name=fullname, email=email, password=password,
+                    mobile=mobile, location=location, supply_capacity=supply_capacity)
     farmer.save()
     return render(request, 'transport/loginPage.html')
 
 
-<<<<<<< HEAD
+<< << << < HEAD
+
+
 def authenticateFarmer(request):
     print("helloo")
     email = request.POST.get('email')
@@ -78,66 +68,76 @@ def authenticateFarmer(request):
     if (response):
         if (check_password(password, response.password)):
             print("successfull")
+
             # return render(request,"transport/bookingPage.html")
-=======
+== == == =
+
+
 def authenticate(request):
     # print("helloo")
-    email=request.POST['email']
-    password=request.POST['password']
-    response=Farmer.objects.filter(email=email,password=password)
+    email = request.POST['email']
+    password = request.POST['password']
+    response = Farmer.objects.filter(email=email, password=password)
     print(response)
-    if(response):
-        return render(request,"transport/bookingPage.html")
->>>>>>> 6100641488d31681c58f3805567b853e76fdbde3
-    print("failed")
-    return render(request,"transport/errorPage.html")
+    if (response):
+        return render(request, "transport/bookingPage.html")
+
+
+>>>>>> > 6100641488d31681c58f3805567b853e76fdbde3
+print("failed")
+ return render(request, "transport/errorPage.html")
 
 
 def registerDriver(request):
-<<<<<<< HEAD
-    fullname = request.POST['fullname']
-    email = request.POST['email']
-    password = request.POST['password']
-    mobile = request.POST['mobile']
+
+
+<< << << < HEAD
+fullname = request.POST['fullname']
+ email = request.POST['email']
+  password = request.POST['password']
+   mobile = request.POST['mobile']
     location = request.POST['location']
     license = request.POST['license']
     # hashed password
     hashed_password = make_password(password)
     driver = Driver(name=fullname, email=email, password=hashed_password,
                     mobile=mobile, location=location, license_no=license)
-=======
-    print("hello")
-    fullname=request.POST['fullname']
-    email=request.POST['email']
-    password=request.POST['password']
-    mobile=request.POST['mobile']
-    rating=request.POST['rating']
-    license=request.POST['license']
-    #hashed password
+== == == =
+print("hello")
+ fullname = request.POST['fullname']
+  email = request.POST['email']
+   password = request.POST['password']
+    mobile = request.POST['mobile']
+    rating = request.POST['rating']
+    license = request.POST['license']
+    # hashed password
     # hashed_password=make_password(password)
-    driver=Driver(name=fullname,email=email,password=password,mobile=mobile,rating=rating,rides_made=0,license_no=license)
->>>>>>> 6100641488d31681c58f3805567b853e76fdbde3
-    driver.save()
-    return render(request, 'transport/loginPageTruck.html')
+    driver = Driver(name=fullname, email=email, password=password,
+                    mobile=mobile, rating=rating, rides_made=0, license_no=license)
+>>>>>> > 6100641488d31681c58f3805567b853e76fdbde3
+driver.save()
+ return render(request, 'transport/loginPageTruck.html')
 
 
 def authenticateDriver(request):
-<<<<<<< HEAD
-    email = request.POST.get('email')
-    password = request.POST.get('password')
-    response = Driver.objects.get(email=email)[:1]
-    if (response):
+
+
+<< << << < HEAD
+email = request.POST.get('email')
+ password = request.POST.get('password')
+  response = Driver.objects.get(email=email)[:1]
+   if (response):
         if (check_password(password, response.password)):
             print("successfull")
             return render(request, "transport/bookingPage.html")
     print("failed")
     return render(request, "transport/errorPage.html")
-=======
-    email=request.POST.get('email')
-    password=request.POST.get('password')
-    response=Driver.objects.filter(email=email,password=password)
-    if(response):
-        return render(request,"transport/bookingPage.html")
-    return render(request,"transport/errorPage.html")
->>>>>>> 4b75e2e29510409485bf80811c3e11861371a95f
->>>>>>> 6100641488d31681c58f3805567b853e76fdbde3
+== == == =
+ email = request.POST.get('email')
+  password = request.POST.get('password')
+   response = Driver.objects.filter(email=email, password=password)
+    if (response):
+        return render(request, "transport/bookingPage.html")
+    return render(request, "transport/errorPage.html")
+>>>>>> > 4b75e2e29510409485bf80811c3e11861371a95f
+>>>>>> > 6100641488d31681c58f3805567b853e76fdbde3
